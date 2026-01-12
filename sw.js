293e01fe65139bb75da4682522fa15f52a9fbfe2
pollyyeh61-eng <pollyyeh61@gmail.com>
2026-01-12 21:40:32 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = 'vitality-hair-v3'; // 每次更新建議跳號
+// 更改版本號為 v4，這會強制瀏覽器發現更新
+const CACHE_NAME = 'vitality-v4'; 
 const ASSETS = [
     'index.html',
     'logo.png',
@@ -9,6 +10,14 @@ self.addEventListener('install', (e) => {
     self.skipWaiting();
     e.waitUntil(
         caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then(keys => Promise.all(
+            keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null)
+        ))
     );
 });
 
